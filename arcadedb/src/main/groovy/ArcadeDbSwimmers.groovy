6b510838e78944bb0d6695fbbdcab05abc97d7d0
Paul King <paulk@asert.com.au>
2024-08-28 22:15:38 +1000

    tugraph example

--- arcadedb/src/main/groovy/ArcadeDbSwimmers.groovy
+++ arcadedb/src/main/groovy/ArcadeDbSwimmers.groovy
@@ -1,7 +1,23 @@
+/*
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import com.arcadedb.database.DatabaseFactory
 import com.arcadedb.gremlin.ArcadeGraph
 
-var factory = new DatabaseFactory("/tmp/swimming")
+var factory = new DatabaseFactory("swimming")
 
 def insertSwimmer(db, name, country) {
     var swimmer = db.newVertex('Swimmer')
@@ -23,7 +39,17 @@ if (factory.exists()) {
     factory.open().drop()
 }
 
-try(var db = factory.create()) {
+/* server is only needed if you want to access studio */
+//var server
+//Thread.start {
+//    var config = new ContextConfiguration()
+//    config.setValue(GlobalConfiguration.SERVER_DATABASE_DIRECTORY, "swimming")
+//    server = new ArcadeDBServer(config)
+//    server.start()
+//}
+
+//var db = server.getOrCreateDatabase('swimming')
+try (var db = factory.create()) {
     db.transaction { ->
         db.schema.with {
             createVertexType('Swimmer')
--- tugraph/src/main/groovy/TugraphSwimmers.groovy
+++ tugraph/src/main/groovy/TugraphSwimmers.groovy
@@ -1,89 +1,106 @@
-import groovy.sql.Sql
 /*
-import org.apache.age.jdbc.base.Agtype
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 
-String DB_URL = "jdbc:postgresql://localhost:5455/postgresDB"
-String USER = "postgresUser"
-String PASS = "postgresPW"
+//import org.neo4j.driver.AuthTokens
+//import org.neo4j.driver.GraphDatabase
+//import org.neo4j.driver.Result
+//import org.neo4j.driver.SessionConfig
+import com.antgroup.tugraph.TuGraphDbRpcClient
+import groovy.transform.Field
 
-Sql.withInstance(DB_URL, USER, PASS, 'org.postgresql.jdbc.PgConnection') { sql ->
-    sql.connection.addDataType("agtype", Agtype)
-    sql.execute("CREATE EXTENSION IF NOT EXISTS age")
-    sql.execute("LOAD 'age'")
-    sql.execute('SET search_path = ag_catalog, "$user", public')
 
-    sql.execute("SELECT drop_graph('swimming_graph', true)")
-    sql.execute("SELECT create_graph('swimming_graph')")
-    sql.execute("SELECT create_vlabel('swimming_graph', 'swimmer')")
-    sql.execute("SELECT create_vlabel('swimming_graph', 'swim')")
-    sql.execute('''
-        SELECT * FROM cypher('swimming_graph', $$ CREATE
-        (es:swimmer {name: 'Emily Seebohm', country: '🇦🇺'}),
-        (swim1:swim {event: 'Heat 4', result: 'First', time: 58.23, where: 'London 2012'}),
-        (es)-[:swam]->(swim1),
+@Field client = new TuGraphDbRpcClient('localhost:9090', "admin", "73@TuGraph")
+//var authToken = AuthTokens.basic("admin", "73@TuGraph")
+//var driver = GraphDatabase.driver("bolt://localhost:7687", authToken)
 
-        (km:swimmer {name: 'Kylie Masse', country: '🇨🇦'}),
-        (swim2:swim {event: 'Heat 4', result: 'First', time: 58.17, where: 'Tokyo 2021'}),
-        (km)-[:swam]->(swim2),
-        (swim2)-[:supersedes]->(swim1),
-        (swim3:swim {event: 'Final', result: '🥈', time: 57.72, where: 'Tokyo 2021'}),
-        (km)-[:swam]->(swim3),
-
-        (rs:swimmer {name: 'Regan Smith', country: '🇺🇸'}),
-        (swim4:swim {event: 'Heat 5', result: 'First', time: 57.96, where: 'Tokyo 2021'}),
-        (rs)-[:swam]->(swim4),
-        (swim4)-[:supersedes]->(swim2),
-        (swim5:swim {event: 'Semifinal 1', result: 'First', time: 57.86, where: 'Tokyo 2021'}),
-        (rs)-[:swam]->(swim5),
-        (swim6:swim {event: 'Final', result: '🥉', time: 58.05, where: 'Tokyo 2021'}),
-        (rs)-[:swam]->(swim6),
-        (swim7:swim {event: 'Final', result: '🥈', time: 57.66, where: 'Paris 2024'}),
-        (rs)-[:swam]->(swim7),
-        (swim8:swim {event: 'Relay leg1', result: 'First', time: 57.28, where: 'Paris 2024'}),
-        (rs)-[:swam]->(swim8),
-
-        (kmk:swimmer {name: 'Kaylie McKeown', country: '🇦🇺'}),
-        (swim9:swim {event: 'Heat 6', result: 'First', time: 57.88, where: 'Tokyo 2021'}),
-        (kmk)-[:swam]->(swim9),
-        (swim9)-[:supersedes]->(swim4),
-        (swim5)-[:supersedes]->(swim9),
-        (swim10:swim {event: 'Final', result: '🥇', time: 57.47, where: 'Tokyo 2021'}),
-        (kmk)-[:swam]->(swim10),
-        (swim10)-[:supersedes]->(swim5),
-        (swim11:swim {event: 'Final', result: '🥇', time: 57.33, where: 'Paris 2024'}),
-        (kmk)-[:swam]->(swim11),
-        (swim11)-[:supersedes]->(swim10),
-        (swim8)-[:supersedes]->(swim11),
-
-        (kb:swimmer {name: 'Katharine Berkoff', country: '🇺🇸'}),
-        (swim12:swim {event: 'Final', result: '🥉', time: 57.98, where: 'Paris 2024'}),
-        (kb)-[:swam]->(swim12)
-        $$) AS (a agtype)
-    ''')
-
-    assert sql.rows('''
-        SELECT * from cypher('swimming_graph', $$
-        MATCH (s:swim)
-        WHERE left(s.event, 4) = 'Heat'
-        RETURN s
-        $$) AS (a agtype)
-    ''').a*.map*.get('properties')*.where.toUnique() == ['London 2012', 'Tokyo 2021']
-
-    assert sql.rows('''
-        SELECT * from cypher('swimming_graph', $$
-        MATCH (s1:swim {event: 'Final'})-[:supersedes]->(s2:swim)
-        RETURN s1
-        $$) AS (a agtype)
-    ''').a*.map*.get('properties')*.time == [57.47, 57.33]
-
-    sql.eachRow('''
-        SELECT * from cypher('swimming_graph', $$
-        MATCH (s1:swim)-[:supersedes]->(swim1)
-        RETURN s1
-        $$) AS (a agtype)
-    ''') {
-        println it.a*.map*.get('properties')[0].with{ "$it.where $it.event" }
-    }
+//var session = driver.session(SessionConfig.forDatabase("default"))
+def callCypher(String s) {
+    client.callCypher(s, "default", 100)
 }
-*/
-var client = new TuGraphDbRpcClient("127.0.0.1:19099", "admin", "73@TuGraph")
+'''
+CALL db.dropDB()
+CALL db.createVertexLabel('Swimmer', 'name', 'name', STRING, false, 'country', STRING, false)
+CALL db.createVertexLabel('Swim', 'id', 'id', INT32, false, 'event', STRING, false, 'result', STRING, false, 'at', STRING, false, 'time', FLOAT, false)
+CALL db.createEdgeLabel('swam','[["Swimmer","Swim"]]')
+CALL db.createEdgeLabel('supersedes','[["Swim","Swim"]]')
+'''.readLines().grep().each(this::callCypher)
+
+callCypher '''create
+    (es:Swimmer {name: 'Emily Seebohm', country: 'AU'}),
+    (swim1:Swim {event: 'Heat 4', result: 'First', time: 58.23, at: 'London 2012', id:1}),
+    (es)-[:swam]->(swim1),
+    (km:Swimmer {name: 'Kylie Masse', country: 'CA'}),
+    (swim2:Swim {event: 'Heat 4', result: 'First', time: 58.17, at: 'Tokyo 2021', id:2}),
+    (km)-[:swam]->(swim2),
+    (swim3:Swim {event: 'Final', result: 'Silver', time: 57.72, at: 'Tokyo 2021', id:3}),
+    (km)-[:swam]->(swim3),
+    (swim2)-[:supersedes]->(swim1),
+    (rs:Swimmer {name: 'Regan Smith', country: 'US'}),
+    (swim4:Swim {event: 'Heat 5', result: 'First', time: 57.96, at: 'Tokyo 2021', id:4}),
+    (rs)-[:swam]->(swim4),
+    (swim5:Swim {event: 'Semifinal 1', result: 'First', time: 57.86, at: 'Tokyo 2021', id:5}),
+    (rs)-[:swam]->(swim5),
+    (swim6:Swim {event: 'Final', result: 'Bronze', time: 58.05, at: 'Tokyo 2021', id:6}),
+    (rs)-[:swam]->(swim6),
+    (swim7:Swim {event: 'Final', result: 'Silver', time: 57.66, at: 'Paris 2024', id:7}),
+    (rs)-[:swam]->(swim7),
+    (swim8:Swim {event: 'Relay leg1', result: 'First', time: 57.28, at: 'Paris 2024', id:8}),
+    (rs)-[:swam]->(swim8),
+    (swim4)-[:supersedes]->(swim2),
+    (kmk:Swimmer {name: 'Kaylie McKeown', country: 'AU'}),
+    (swim9:Swim {event: 'Heat 6', result: 'First', time: 57.88, at: 'Tokyo 2021', id:9}),
+    (kmk)-[:swam]->(swim9),
+    (swim9)-[:supersedes]->(swim4),
+    (swim5)-[:supersedes]->(swim9),
+    (swim10:Swim {event: 'Final', result: 'Gold', time: 57.47, at: 'Tokyo 2021', id:10}),
+    (kmk)-[:swam]->(swim10),
+    (swim10)-[:supersedes]->(swim5),
+    (swim11:Swim {event: 'Final', result: 'Gold', time: 57.33, at: 'Paris 2024', id:11}),
+    (kmk)-[:swam]->(swim11),
+    (swim11)-[:supersedes]->(swim10),
+    (swim8)-[:supersedes]->(swim11)
+'''
+
+callCypher '''create
+    (kb:Swimmer {name: 'Katharine Berkoff', country: 'UA'}),
+    (swim12:Swim {event: 'Final', result: 'Bronze', time: 57.98, at: 'Paris 2024', id:12}),
+    (kb)-[:swam]->(swim12)
+'''
+
+assert callCypher('''
+    MATCH (s:Swim)
+    WHERE s.event STARTS WITH 'Heat'
+    RETURN DISTINCT s.at AS at
+''') == '[{"at":"London 2012"},{"at":"Tokyo 2021"}]'
+
+assert callCypher('''
+    MATCH (s1:Swim {event: 'Final'})-[:supersedes]->(s2:Swim)
+    RETURN s1.time as time
+''') == '[{"time":57.47},{"time":57.33}]'
+
+assert callCypher('''
+    MATCH (s1:Swim)-[:supersedes*1..10]->(s2:Swim)
+    WHERE s2.at = 'London 2012'
+    RETURN s1.at as at, s1.event as event
+''') == '''
+    [{"at":"Tokyo 2021","event":"Heat 4"},
+     {"at":"Tokyo 2021","event":"Heat 5"},
+     {"at":"Tokyo 2021","event":"Semifinal 1"},
+     {"at":"Paris 2024","event":"Relay leg1"},
+     {"at":"Tokyo 2021","event":"Heat 6"},
+     {"at":"Tokyo 2021","event":"Final"},
+     {"at":"Paris 2024","event":"Final"}]
+'''.readLines()*.trim().join()
