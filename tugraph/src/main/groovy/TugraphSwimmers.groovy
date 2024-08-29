8eebd69cefc2d448475d05eaae9ee1dfa4180c5f
Paul King <paulk@asert.com.au>
2024-08-29 11:04:12 +1000

    minor tweaks

--- tugraph/src/main/groovy/TugraphSwimmers.groovy
+++ tugraph/src/main/groovy/TugraphSwimmers.groovy
@@ -19,7 +19,7 @@ import org.neo4j.driver.GraphDatabase
 import org.neo4j.driver.SessionConfig
 //import com.antgroup.tugraph.TuGraphDbRpcClient
 
-// setup (or run outside docker if you prefer):
+// setup (or choose different docker image or run outside docker if you prefer):
 // docker run -it -d -p 8000:8000 -p 7687:7687 -p 9090:9090 --name tugraph_demo tugraph/tugraph-runtime-ubuntu18.04 lgraph_server
 
 //var client = new TuGraphDbRpcClient('localhost:9090', "admin", "73@TuGraph")
