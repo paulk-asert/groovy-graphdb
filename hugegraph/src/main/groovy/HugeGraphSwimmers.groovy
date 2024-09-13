972e57104c2858d11f3f4e4aa3a5d884dbf43f4f
Paul King <paulk@asert.com.au>
2024-09-14 00:07:53 +1000

    slight simplification

--- hugegraph/src/main/groovy/HugeGraphSwimmers.groovy
+++ hugegraph/src/main/groovy/HugeGraphSwimmers.groovy
@@ -128,12 +128,9 @@ var successInParis = gremlin.gremlin('''
 ''').execute()
 assert successInParis.data() == ['🇦🇺', '🇺🇸']
 
-// shorthands might be possible if the following class is included in the
-// ImportGremlinPlugin.classImports section of the hugegraph server configuration:
-// org.apache.hugegraph.traversal.optimize.Text
 var recordSetInHeat = gremlin.gremlin('''
     g.V().hasLabel('Swim')
-        .filter { it.get().value('event').startsWith('Heat') }
+        .filter(values('event').is(Text.contains('Heat')))
         .values('at').dedup().order()
 ''').execute()
 assert recordSetInHeat.data() == ['London 2012', 'Tokyo 2021']
